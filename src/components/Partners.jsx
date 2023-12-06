53de3836d117b5b734babc1670e4581c81406bca
jamatov <mirabdullajamatov@gmail.com>
2023-12-06 22:15:26 +0500

    partners new logo

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
-import logo1 from '../images/partner1.svg'
+import logo1 from '../images/partners1.svg'
+import logo2 from '../images/partners2.svg'
+import logo3 from '../images/partners3.png'
+import logo4 from '../images/partners4.svg'
+import logo5 from '../images/partners5.svg'
+import logo6 from '../images/partners6.svg'
+import logo7 from '../images/partners7.svg'
+import logo8 from '../images/partners8.svg'
+
+
+
 
 export default function Partners() {
   return (
@@ -7,32 +17,55 @@ export default function Partners() {
       <div className="container">
         <div className="row">
           <h2><span>Наши</span> довольные <br /> клиенты</h2>
-          <div className="col-md-3 number-block">
+          <div className="col-md-3 number-block mb-3">
             <div className="card">
               <img src={logo1} alt="logo" />
             </div>
           </div>
 
-          <div className="col-md-3 number-block">
+          <div className="col-md-3 number-block mb-3">
             <div className="card">
-              <h3>1200</h3>
-              <p>Постоянных клиентов</p>
+              <img src={logo2} alt="logo" />
             </div>
           </div>
 
-          <div className="col-md-3 number-block">
+          <div className="col-md-3 number-block mb-3">
             <div className="card">
-              <h3>25.000</h3>
-              <p>Почишено м²</p>
+              <img className='qush' src={logo3} alt="logo" />
             </div>
           </div>
 
-          <div className="col-md-3 number-block">
+          <div className="col-md-3 number-block mb-3">
             <div className="card">
-              <h3>78</h3>
-              <p>Онлайн заказов</p>
+              <img src={logo4} alt="logo" />
             </div>
           </div>
+
+          <div className="col-md-3 number-block mb-3">
+            <div className="card">
+              <img src={logo5} alt="logo" />
+            </div>
+          </div>
+
+          <div className="col-md-3 number-block mb-3">
+            <div className="card">
+              <img src={logo6} alt="logo" />
+            </div>
+          </div>
+
+          <div className="col-md-3 number-block mb-3">
+            <div className="card">
+              <img src={logo7} alt="logo" />
+            </div>
+          </div>
+
+          <div className="col-md-3 number-block mb-3">
+            <div className="card">
+              <img src={logo8} alt="logo" />
+            </div>
+          </div>
+
+         
 
           
         </div>
